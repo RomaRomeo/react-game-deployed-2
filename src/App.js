8379fd45395ae7a5efbb22ca24cc10a24682ab02
RomaRomeo <r.pidluzhnyi@ukr.net>
2020-09-02 15:15:43 +0300

    react-deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Board from './components/board-component/board-component'
+import initializeDeck from './deck'
 
-function App() {
+export default function App() {
+  const [cards, setCards] = useState([])
+  const [flipped, setFlipped] = useState([])
+  const [dimension, setDimension] = useState(400)
+  const [solved, setSolved] = useState([])
+  const [disabled, setDisabled] = useState(false)
+
+  useEffect(() => {
+    resizeBoard()
+    setCards(initializeDeck())
+  }, [])
+
+  useEffect(() => {
+    const resizeListener = window.addEventListener('resize', resizeBoard)
+
+    return () => window.removeEventListener('resize', resizeListener)
+  })
+
+  const handleClick = (id) => {
+    setDisabled(true)
+    if (flipped.length===0) {
+    setFlipped([id])
+    setDisabled(false)
+    } else {
+      if (sameCardClicked(id)) return
+      setFlipped([flipped[0], id])
+      if (isMatch(id)) {
+        setSolved([...solved, flipped[0], id])
+        resetCards()
+      } else {
+        setTimeout(resetCards, 2000)
+      }
+    }
+  }  
+
+  const resetCards = () => {
+    setFlipped([])
+    setDisabled(false)
+  }
+
+  const sameCardClicked = (id) => flipped.includes(id)
+
+  const isMatch = (id) => {
+    const clickedCard = cards.find((card) => card.id === id)
+    const flippedCard = cards.find((card) => flipped[0] === card.id)
+    return flippedCard.type === clickedCard.type
+  }
+
+  const resizeBoard = () => {
+    setDimension(
+      Math.min(
+        document.documentElement.clientWidth,
+        document.documentElement.clientHeight,
+      ),
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Memory Game</h1>
+      <h2>Can you remember where this cards are?</h2>
+
+      <Board
+        dimension={dimension}
+        cards={cards}
+        flipped={flipped}
+        handleClick={handleClick}
+        disabled={disabled}
+        solved={solved}
+        />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/card-component/card-component.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './card-component.css'
+
+export default function Card({
+    handleClick,
+    id,
+    type,
+    flipped,
+    solved,
+    height,
+    width,
+    disabled,
+  }) {
+    return (
+      <div
+        className={`flip-conteiner ${flipped ? 'flipped' : ''}`}
+        style={{
+          width,
+          height,
+        }}
+        onClick={() => disabled ? null : handleClick(id)}
+      >
+        <div className='flipper'>
+          <img 
+            style={{
+              width,
+              height,
+            }} alt = 'img'
+            className={flipped ? 'front' : 'back'}
+            src={flipped || solved ? `/img/${type}.png` : `/img/back.png`}
+          />
+        </div>
+      </div>
+    );
+}
+
+Card.propTypes = {
+    handleClick: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
+    flipped: PropTypes.bool.isRequired,
+    solved: PropTypes.bool.isRequired,
+    type: PropTypes.string.isRequired,
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
+    disabled: PropTypes.bool.isRequired,
+
+}
+
