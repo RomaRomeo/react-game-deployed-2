aab97dab61d7cfda7690f6ab51472259ae87b7db
RomaRomeo <r.pidluzhnyi@ukr.net>
2020-09-02 15:16:15 +0300

    Updates

---
+++ precache-manifest.f01783d40a2879878b69f0f4a57f8ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74972c404d6c3547fc901a11474213a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a71a2329cf85b99803e1",
+    "url": "/static/css/main.6542bd51.chunk.css"
+  },
+  {
+    "revision": "17dfa8ee3e9197bbb7c7",
+    "url": "/static/js/2.d5486da2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5486da2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a71a2329cf85b99803e1",
+    "url": "/static/js/main.da94fab5.chunk.js"
+  },
+  {
+    "revision": "53ff9637c311bd424e1b",
+    "url": "/static/js/runtime-main.5a7750ca.js"
+  }
+]);
